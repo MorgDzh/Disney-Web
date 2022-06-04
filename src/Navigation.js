dfb076766299fd8f10221e03b1dfe695af516b9f
MorgDzh <dzhanaliev.aktan@gmail.com>
2022-06-04 22:53:58 +0600

    added navbar

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Login from "./components/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Header from "./components/Header";
 
 const Navigation = () => {
   return (
     <BrowserRouter>
+      <Header />
       <Routes>
         <Route path="/" element={<Login />} />
       </Routes>
