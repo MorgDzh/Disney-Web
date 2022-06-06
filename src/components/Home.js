03e326748c0d5be0ba6081084e7b4da4a401100a
MorgDzh <dzhanaliev.aktan@gmail.com>
2022-06-06 18:45:47 +0600

    added slick-carousel

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,13 +1,10 @@
 import styled from "styled-components";
-
-import React from "react";
+import ImgSlider from "./ImgSlider";
 
 const Home = (props) => {
   return (
     <Container>
-      <h2>Hello</h2>
-      <h2>Hello</h2>
-      <h2>Hello</h2>
+      <ImgSlider />
     </Container>
   );
 };
@@ -17,9 +14,8 @@ const Container = styled.main`
   min-height: calc(100vh - 250px);
   overflow-x: hidden;
   display: block;
-  top: 72;
-  padding: 0 calc(2.5vw + 5px);
-
+  top: 72px;
+  padding: 0 calc(3.5vw + 5px);
   &:after {
     background: url("/images/home-background.png") center center / cover
       no-repeat fixed;
