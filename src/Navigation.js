efeb6adb8a377d9e12a1f64a52bd4c5f0a2d957c
MorgDzh <dzhanaliev.aktan@gmail.com>
2022-06-06 18:34:51 +0600

    create home page

--- src/Navigation.js
+++ src/Navigation.js
@@ -1,7 +1,8 @@
 import React from "react";
-import Login from "./components/Login";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 const Navigation = () => {
   return (
@@ -9,6 +10,7 @@ const Navigation = () => {
       <Header />
       <Routes>
         <Route path="/" element={<Login />} />
+        <Route path="/home" element={<Home />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/components/Home.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+
+import React from "react";
+
+const Home = (props) => {
+  return (
+    <Container>
+      <h2>Hello</h2>
+      <h2>Hello</h2>
+      <h2>Hello</h2>
+    </Container>
+  );
+};
+
+const Container = styled.main`
+  position: relative;
+  min-height: calc(100vh - 250px);
+  overflow-x: hidden;
+  display: block;
+  top: 72;
+  padding: 0 calc(2.5vw + 5px);
+
+  &:after {
+    background: url("/images/home-background.png") center center / cover
+      no-repeat fixed;
+    content: "";
+    position: absolute;
+    inset: 0px;
+    opacity: 1;
+    z-index: -1;
+  }
+`;
+
+export default Home;
