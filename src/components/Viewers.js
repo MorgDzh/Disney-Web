0a5e7bdd0acc2d8a8438772da8a10789f6d1b49e
MorgDzh <dzhanaliev.aktan@gmail.com>
2022-06-08 23:07:40 +0600

    styled viewers

--- src/components/Viewers.js
+++ src/components/Viewers.js
@@ -1,25 +1,51 @@
-import React from 'react'
-import styled from "styled-components"
+import React from "react";
+import styled from "styled-components";
 
 const Viewers = (props) => {
   return (
     <Container>
-        <div>1</div>
-        <div>2</div>
-        <div>3</div>
-        <div>4</div>
-        <div>5</div>
+      <Wrap>
+        <img src="/images/viewers-disney.png" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-pixar.png" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-marvel.png" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-starwars.png" alt="" />
+      </Wrap>
+      <Wrap>
+        <img src="/images/viewers-national.png" alt="" />
+      </Wrap>
     </Container>
-  )
-}
+  );
+};
 
 const Container = styled.div`
-    margin: 30px;
-    padding: 30px 0px 26px;
-    display: grid;
-    grid-gap: 25px;
-    gap: 25px;
-    grid-template-columns: repeat(5, minmax(0, 1fr));
-`
+  margin: 30px;
+  padding: 30px 0px 26px;
+  display: grid;
+  grid-gap: 25px;
+  gap: 25px;
+  grid-template-columns: repeat(5, minmax(0, 1fr));
 
-export default Viewers
+  @media (max-width: 768px) {
+    grid-template-columns: repeat(1, minmax(0, 1fr));
+  }
+`;
+
+const Wrap = styled.div`
+  padding-top: 56.25%;
+  border-radius: 10px;
+  box-shadow: rgb(0 0 0 / 69%) 0px 26px 30px -10px,
+    rgb(0 0 0 / 73%) 0px 16px 10px -10px;
+  cursor: pointer;
+  overflow: hidden;
+  position: relative;
+  transition: all 250ms cubic-bezier(0.25, 0,46, 0,45, 0.94) 0s;
+  border: 3px solid rgba(249, 249, 249, 0.1);
+`;
+
+export default Viewers;
