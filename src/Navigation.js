f463ce197a38f5a0f6d0192738cd712a40919183
MorgDzh <dzhanaliev.aktan@gmail.com>
2022-06-09 19:56:00 +0600

    added details for movie

--- src/Navigation.js
+++ src/Navigation.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Login from "./components/Login";
 import Header from "./components/Header";
 import Home from "./components/Home";
+import Detail from "./components/Detail";
 
 const Navigation = () => {
   return (
@@ -11,6 +12,7 @@ const Navigation = () => {
       <Routes>
         <Route path="/" element={<Login />} />
         <Route path="/home" element={<Home />} />
+        <Route path="/detail/:id" element={<Detail />} />
       </Routes>
     </BrowserRouter>
   );
